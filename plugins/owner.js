ff4c6451e2a763a903a6d37e1d6736df8a715444
Moaaz282-bit <moaazmego9988@gmail.com>
2024-09-27 12:52:01 +0300

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -3,14 +3,14 @@ let handler = async (m, { conn }) => {
   let caption = `
 *「 معلومات عن صاحب البوت 」*
 
-*Number :*\nwa.me/212717457920
-*instagram:*\ninstagram.com/noureddine_ouafy
+*Number :*\nwa.me/201550140927
+*name   :ميجو العقرب الاصلي الاكبر/كبير تيم العقارب
 
-*youtube:*\nyoutube.com/@noureddineouafy2
 
-*facebook page:*\nwww.facebook.com/profile.php?id=100063533185520
 
-*script bot :* github.com/noureddineouafy
+
+
+
 
 `.trim()
   m.reply(caption)
